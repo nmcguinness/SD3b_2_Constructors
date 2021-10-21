261c286ef0f7f6cd98cd13d1c0ce50e739968eb3
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-21 09:24:36 +0100

    Added cout to constructors and destructors

--- SD3b_2_Constructors/SD3b_2_Constructors.cpp
+++ SD3b_2_Constructors/SD3b_2_Constructors.cpp
@@ -9,16 +9,20 @@ private:
 	int x, y;
 
 public:
-	Point2D(int x, int y) : x(x), y(y) {}
-	~Point2D() {}
+	Point2D(int x, int y) : x(x), y(y) {
+		cout << "constructing..." << endl;
+	}
+	~Point2D() {
+		cout << "destructing..." << endl;
+	}
 
+#pragma region Get/Set & toString
 	int getX() const { return x; }
 	int getY() const { return y; }
-
 	string toString() const { return x + "," + y; }
+#pragma endregion
 };
 
 int main()
 {
-	std::cout << "Hello World!\n";
 }
